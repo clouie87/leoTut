3297587be7fd2f0a84fe734da9923836bee57681
clouie87 <carol.louie@gmail.com>
2015-10-19 19:35:48 -0300

    have firebase connected

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,24 +1,27 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, Products) {
+  var items = Products.all;
+    console.log('the products are', items);
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  })
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+.controller('ChatsCtrl', function($scope, Products) {
+  $scope.image = "http://ak-hdl.buzzfed.com/static/2015-03/19/11/enhanced/webdr05/grid-cell-15754-1426777975-0.jpg";
+
+  $scope.button = function(product) {
+    console.log('button was click', product);
+    //$scope.product.image = "http://ak-hdl.buzzfed.com/static/2015-03/19/11/enhanced/webdr05/grid-cell-15754-1426777975-0.jpg";
+    Products.saveProduct(product, $scope.image);
+    //send to database. create a service to send to database.
+
+
+
+  }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+  //$scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,35 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Products', function(FURL, $firebaseArray) {
   // Might use a resource here that returns a JSON array
+    var ref = new Firebase(FURL);
+    var products = $firebaseArray(ref.child('products'));
+
+
+
 
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+  var Products = {
+    all: products,
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
+    saveProduct: function(product, image){
+      console.log('will save this to the database', product);
+      var newProduct = {
+        name :product.name,
+        price: product.price,
+        image: image
+      };
+
+      return products.$add(newProduct).then(function(){
+        //toaster.pop('success', "You created a new challenge!")
+        console.log('added to db');
+      });
+      console.log(newProduct);
+
     }
+
   };
+    return Products;
+
+
 });
